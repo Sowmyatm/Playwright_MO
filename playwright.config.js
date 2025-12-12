1e1fd19f072c04415c7df0bd8f6e37fb9d42fdc5
Sowmyatm <sowmya.prasi@gmail.com>
2025-12-11 16:12:29 -0800

    added yml file

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'],['allure-playwright']],
+  reporter: [['html'],['allure-playwright'],['github']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
